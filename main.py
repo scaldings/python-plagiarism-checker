6670759b8d5fd6bd9103fbb621d965556ea9ed1b
Luke <lukas.zahoransky@zsbystricany.info>
2020-10-15 17:59:28 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,34 @@
+from googlesearch import search
+from bs4 import BeautifulSoup
+import requests
+
+
+def plagiarism_check(qry: str):
+    x_searches = []
+    for x in search(query=qry, num=10, stop=10, pause=2.0):
+        x_searches.append(x)
+
+    for x in range(0, len(x_searches) - 1):
+        url = x_searches[x]
+        response = requests.get(url)
+
+        soup = BeautifulSoup(response.text, 'html.parser')
+        all_p = soup.find_all('p')
+
+        for y in range(0, len(all_p)):
+            if qry in str(all_p[y]):
+                return url
+
+
+def main():
+    query = input('Enter the text you want to search for: ')
+    link = plagiarism_check(query)
+
+    if link is None:
+        print('No link has been found.')
+    else:
+        print(f'A similarity has been found: {link}')
+
+
+if __name__ == '__main__':
+    main()
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='plagiarism',
+    version='1.0',
+    packages=[''],
+    url='https://github.com/scaldings/simple-plagiarism-checker.git',
+    license='',
+    author='scaldings',
+    author_email='',
+    description=''
+)

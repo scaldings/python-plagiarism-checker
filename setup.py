15ec65302baa4b8d12f732d3eb7f52e0850fad9e
Luke <lukas.zahoransky@zsbystricany.info>
2020-10-25 23:09:13 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ setup(
     name='plagiarism',
     version='1.0',
     packages=[''],
-    url='https://github.com/scaldings/simple-plagiarism-checker.git',
+    url='https://github.com/scaldings/simple-plagiarism-checker',
     license='',
     author='scaldings',
     author_email='',
-    description=''
+    description='A python script that checks if the given text is taken from a website'
 )
